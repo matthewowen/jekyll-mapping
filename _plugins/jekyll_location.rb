03a908c51e2a68c7abd1eab2ebfd08a643252150
Matthew Owen <matthew.owen95@gmail.com>
2012-07-07 12:28:20 +0100

    Initial commit Put in the basics of creating a liquid tag which uses latitude and longitude set on a page to render out a map

---
+++ _plugins/jekyll_location.rb
@@ -0,0 +1,19 @@
+module Jekyll
+    class MapTag < Liquid::Tag
+        def initialize(tag_name, text, tokens)
+            @config = Jekyll.configuration({})['mapping']
+            @engine = @config['provider']
+            @key = @config['api_key']
+            @zoom = @config['zoom']
+            super
+        end
+
+        def render(context)
+            @latitude = context['page']['latitude']
+            @longitude = context['page']['longitude']
+            "<img src=\"http://maps.googleapis.com/maps/api/staticmap?markers=#{@latitude},#{@longitude}&size=500x400&zoom=#{@zoom}&sensor=false\">"
+        end
+    end
+end
+
+Liquid::Template.register_tag('render_map', Jekyll::MapTag)
