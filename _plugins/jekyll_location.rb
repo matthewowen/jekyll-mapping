ffbcfd4ca736736992f533af208ea034a31d22ce
Matthew Owen <matthew.owen95@gmail.com>
2012-07-07 18:29:41 +0100

    Properly configurable dimensions

--- _plugins/jekyll_location.rb
+++ _plugins/jekyll_location.rb
@@ -7,11 +7,16 @@ module Jekyll
             @zoom = @config['zoom']
             @textif = text
             if text == ""
-                @width = "600"
-                @height = "400"
+                if @config['dimensions']
+                    @width = @config['dimensions']['width']
+                    @height = @config['dimensions']['height']
+                else
+                    @width = '600'
+                    @height = '400'
+                end
             else
-                @width = text.split(",").first
-                @height = text.split(",").last
+                @width = text.split(",").first.strip
+                @height = text.split(",").last.strip
             end
             super
         end
@@ -22,7 +27,7 @@ module Jekyll
             if @engine == 'google_static'
                 return "<img src=\"http://maps.googleapis.com/maps/api/staticmap?markers=#{@latitude},#{@longitude}&size=#{@width}x#{@height}&zoom=#{@zoom}&sensor=false\">"
             elsif @engine == 'google_js' || 'openstreetmap'
-                return "<div id=\"jekyll-mapping\"></div>"
+                return "<div id=\"jekyll-mapping\" style=\"height:#{@height}px;width:#{@width}px;\"></div>"
             end
         end
     end
